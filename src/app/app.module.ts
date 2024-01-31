db2fd7b4934a3c83e3436cc3c1a160e4eb24b30a
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-01-31 13:02:59 -0500

    Highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HightlightDirective } from './hightlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HightlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hightlight.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef,HostListener } from '@angular/core';
+
+@Directive({
+
+  selector: '[appHightlight]'
+})
+export class HightlightDirective {
+ 
+  constructor(private el:ElementRef) { 
+      
+    }
+
+    @HostListener('mouseenter') onMouseEnter() {
+      this.highlight('yellow');
+    }
+    
+    @HostListener('mouseleave') onMouseLeave() {
+      this.highlight('');
+    }
+    
+    private highlight(color: string) {
+      this.el.nativeElement.style.backgroundColor = color;
+    }
+  }
